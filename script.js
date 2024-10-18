111b3ffdad79a12aec3d58c5881e1549d8ad9550
tjw98 <149226097+tjw98@users.noreply.github.com>
2024-10-18 23:03:05 +1000

    feat: add createGrid function
    - Implemented createGrid function that creates a grid layout by
    generating multiple divs
    - Assigns each div a class of "square"

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
+function createGrid() {
+  const squares = [];
+
+  for (let i = 0; i < 256; i++) {
+    squares[i] = document.createElement("div");
+    mainContainer.appendChild(squares[i]);
+    squares[i].classList.add(".square");
+  }
+}
+
 const mainContainer = document.querySelector(".main-container");
-mainContainer.textContent = "Hi";
+createGrid();
