2bee735bd8c6c36f54062078f95c815b807aefb6
tjw98 <149226097+tjw98@users.noreply.github.com>
2024-10-19 00:27:27 +1000

    Feat: add event listener for user prompt
    - user input for grid size now stored in gridLength
    - button now resets input box once pressed

--- script.js
+++ script.js
@@ -3,19 +3,32 @@ function createGrid() {
 
   for (let i = 0; i < 256; i++) {
     squares[i] = document.createElement("div");
+    squares[i].style.height = `${sketchPadPx / (Math.sqrt(256))}px`;
+    squares[i].style.width = `${sketchPadPx / (Math.sqrt(256))}px`;
     mainContainer.appendChild(squares[i]);
     squares[i].classList.add("square");
   }
 }
 
+const sketchPadPx = 600;
 const mainContainer = document.querySelector(".main-container");
-createGrid();
+const promptBtn = document.querySelector("button");
+const input = document.querySelector("input");
+let gridLength = 16;
 
+// Event listeners to change hover effect on grid
 mainContainer.addEventListener("mouseover", (e) => {
   e.target.style.backgroundColor = "black";
-  
 });
 
 mainContainer.addEventListener("mouseout", (e) => {
   e.target.style.backgroundColor = "#E0E0E0";
 });
+
+// Event listener to take user input
+promptBtn.addEventListener("click", () => {
+  gridLength = input.value;
+  input.value = "";
+});
+
+createGrid();
